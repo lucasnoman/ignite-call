9f5cdc92e5cf7017e5447cb2a2c9d1b4dcd70666
Lucas Calhau Noman <lucasnoman.prestserv@petrobras.com.br>
2024-04-12 13:34:00 -0300

    refactor(Setup & estrutura): Valor inicial do nome de usuário
    Adicionar navegação de roteador e tratamento de envio de formulário no ClaimUsernameForm e preencher previamente o nome de usuário no formulário de registro a partir da consulta

--- src/pages/home/components/ClaimUsernameForm.tsx
+++ src/pages/home/components/ClaimUsernameForm.tsx
@@ -1,9 +1,10 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Text, TextInput } from '@ignite-ui/react'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
-import { Form, FormAnnotation } from './styles'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormAnnotation } from './styles'
 
 const claimUsernameFormSchema = z.object({
   username: z
@@ -21,13 +22,22 @@ export function ClaimUsernameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
   })
 
+  const router = useRouter()
+
+  /* O `router` é uma Promise, logo, leva um tempo para ser executado
+   * Porém o uso do async/await aqui ajuda também o `isSubmitting` do `useForm`,
+   * uma vez que faz com que o `isSubmitting` espere a finalização da execução
+   * dessa função para alterar seu estado para `true` e liberar o `Button`.
+   */
   async function handleClaimUsername(data: any) {
-    console.log(data)
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
   }
 
   return (
@@ -39,11 +49,13 @@ export function ClaimUsernameForm() {
           placeholder="seu-usuario"
           {...register('username')}
         />
-        <Button size="sm" type="submit">
+
+        <Button size="sm" type="submit" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
       </Form>
+
       <FormAnnotation>
         <Text size="sm">
           {errors.username
